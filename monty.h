795dd67d1c4ce6a6a2bc369f870f08a9cc59fa98
The-alpha-male <ronnymunene@outlook.com>
2023-10-19 14:43:35 -0700

    LIFO, FIFO

--- monty.h
+++ monty.h
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <ctype.h>
 
+
 /* Structures */
 /**
  * struct global_s - global variables
@@ -52,6 +53,32 @@ typedef struct cmd_s
 
 extern cmd_t cmd;
 extern int value;
+
+**
+ * struct vars_s - holds all variables to be passed
+ * @IFO: flag to determine stack(0) or queue(1)
+ * @fname: string holding file name
+ * @fp: file pointer to open file
+ * @tokened: tokenized string of our input from file
+ * @head: head of our stack
+ * @line_number: line number that was just read from file
+ * @buf: buffer for the line in the file
+ *
+ * Description: A struct that we make global to pass variables
+ *  for stack, queues, LIFO, FIFO holberton project
+ */
+typedef struct vars_s
+{
+	int IFO;
+	char *fname;
+	FILE *fp;
+	char **tokened;
+	char *buf;
+	struct stack_s *head;
+	unsigned int line_number;
+} vars_t;
+
+extern vars_t *element;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -111,31 +138,38 @@ void free_all(void);
 void error_check(int argc, char *argv[]);
 void error_check2(char *line, unsigned int line_number);
 
-/*Trial*/
-void execute(char *argv);
-int get_opc(stack_t **stack, char *arg, char *val, int line_number);
-void push(stack_t **stack, unsigned int line_number);
+/*trial*/
+/* Stack functions stackfunc_1.c */
+stack_t *add_stack_init(void);
+stack_t *add_stack_end(void);
+void get_tokens(char *buf);
 void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+/* More stack functions stackfunc_2.c */
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+/* Calculation functions calc.c */
 void add(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
 void divide(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
-int check_push(char *token);
-int get_value(char *token);
-char get_token(char *op, char *token);
-void _free(stack_t *stack);
-void clean_stack(stack_t **stack);
-void usage_error(void);
-void open_error(char *file);
-void push_error(FILE *fd, char *line, stack_t *stack, int line_number);
-void instr_error(FILE *fd, char *line, stack_t *stack, char *val, int line_number);
-int _isdigit(char *c);
-stack_t *createNode(int n);
+/* Function finder opcode_search.c */
+void opcode_search(void);
+/* Stack function in stackfunc_3.c */
+void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+/* Exit, free, and error handling error in free_stack.c */
+void free_buffer(void);
+void free_token(void);
+void free_list(stack_t *head);
+void exit_function(unsigned int err_num);
+/* Changes between Stack and Queue in lifo_or_fifo.c */
+void lifo(stack_t **stack, unsigned int line_number);
+void fifo(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
